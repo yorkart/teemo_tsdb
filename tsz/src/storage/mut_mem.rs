776f061bb262f90bf5067310732c45e68134b7c2
wangyue1 <wangyue1@ly.com>
2020-01-07 19:07:35 +0800

    .

--- tsz/src/storage/mut_mem.rs
+++ tsz/src/storage/mut_mem.rs
@@ -1,54 +1,52 @@
 use crate::stream::{BufferedWriter, BufferedReader};
 use std::sync::{RwLock, Arc};
 use std::collections::HashMap;
-use crate::DataPoint;
+use crate::{DataPoint, StdEncoder, StdDecoder};
+use std::cell::RefCell;
 
 #[derive(Debug)]
-pub struct AppendOnlyBlock {
-    time_begin: u32,
-    time_end: u32,
-    bytes_rw: Arc<RwLock<Vec<u8>>>,
+pub struct AppendOnlyBlock<'a> {
+    time_begin: u64,
+    time_end: u64,
+    bytes_rw: Vec<u8>,
+
+    encoder: StdEncoder<BufferedWriter<'a>>,
+    decoder: StdDecoder<BufferedReader<'a>>,
 }
 
-impl AppendOnlyBlock {
-    pub fn new(time_begin: u32, time_end: u32) -> Self {
+impl<'a> AppendOnlyBlock<'a> {
+    pub fn new(time_begin: u64, time_end: u64) -> Self {
         let mut bytes: Vec<u8> = Vec::new();
         let writer = BufferedWriter::new(bytes.as_mut());
-        let rw = RwLock::new(bytes);
-        let bytes_rw = Arc::new(rw);
+        let encoder = StdEncoder::new(time_begin, writer);
+
+        let reader = BufferedReader::new(bytes.as_ref());
+        let decoder = StdDecoder::new(reader);
+
         AppendOnlyBlock {
             time_begin,
             time_end,
-            bytes_rw,
+            bytes_rw: bytes,
+            encoder,
+            decoder,
         }
     }
 
     pub fn len(&self) -> usize {
-        self.bytes_rw.read().unwrap().len()
-    }
-}
-
-impl Clone for AppendOnlyBlock {
-    fn clone(&self) -> Self {
-        let clone = self.bytes_rw.clone();
-        AppendOnlyBlock {
-            time_begin: self.time_begin,
-            time_end: self.time_end,
-            bytes_rw: clone,
-        }
+        self.bytes_rw.len()
     }
 }
 
 #[derive(Debug)]
 pub struct ClosedBlock {
-    time_begin: u32,
-    time_end: u32,
+    time_begin: u64,
+    time_end: u64,
     bytes: Arc<Vec<u8>>,
 }
 
 impl ClosedBlock {
     pub fn new(append_only_block: AppendOnlyBlock) -> Self {
-        let bytes = append_only_block.bytes_rw.read().unwrap().to_vec();
+        let bytes = append_only_block.bytes_rw.to_vec();
         ClosedBlock {
             time_begin: append_only_block.time_begin,
             time_end: append_only_block.time_end,
@@ -58,23 +56,23 @@ impl ClosedBlock {
 }
 
 pub struct TS {
-    append_only_block: AppendOnlyBlock,
+    append_only_block: RefCell<AppendOnlyBlock>,
     closed_blocks: Vec<ClosedBlock>,
 }
 
 impl TS {
     pub fn new(time_begin: u32, bytes: Vec<u8>) -> Self {
         TS {
-            append_only_block: AppendOnlyBlock::new(0, 0),
+            append_only_block: RefCell::new(AppendOnlyBlock::new(0, 0)),
             closed_blocks: Vec::new(),
         }
     }
 
     pub fn append(&self, data_point: DataPoint) {
-        self.append_only_block.bytes_rw.write().unwrap();
+//        self.append_only_block.bytes_rw.
     }
 }
 
 pub struct TSMap {
-    ts_map : std::collections::HashMap<String, TS>,
+    ts_map: std::collections::HashMap<String, TS>,
 }
