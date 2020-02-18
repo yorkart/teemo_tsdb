21dbc03f8f6c40e4f1c0fd1daffe469fff95dbfa
wangyue1 <wangyue1@ly.com>
2020-02-18 09:43:08 +0800

    tsz-rs mod

---
+++ components/tsz-rs/src/encode/mod.rs
@@ -0,0 +1,11 @@
+use crate::DataPoint;
+
+/// Encode
+///
+/// Encode is the trait used to encode a stream of `DataPoint`s.
+pub trait Encode {
+    fn encode(&mut self, dp: DataPoint);
+    fn close(self) -> Box<[u8]>;
+}
+
+pub mod std_encoder;
